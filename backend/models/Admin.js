61532ea1054e820d994862ca2f69acaf5a138891
kathirvel962 <kathirvel.s2024cse@sece.ac.in>
2025-12-18 21:01:35 +0530

    Delete backend directory

--- backend/models/Admin.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-
-const AdminSchema = new mongoose.Schema({
-  username: { type: String, unique: true },
-  password: String,
-  role: { type: String, default: "admin" }
-});
-
-module.exports = mongoose.model("Admin", AdminSchema);
